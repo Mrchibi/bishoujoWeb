94a85ce7f43e3cc3ac17797cfde00dd49933bad5
jianyu liu <bb1034084869@163.com>
2020-10-04 22:48:56 +0800

    初始化

---
+++ bishoujoWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(bishoujoWeb.Startup))]
+namespace bishoujoWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
